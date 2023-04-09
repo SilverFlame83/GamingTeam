6aefdabd994628c30f76fcc5c7be3a9a57fc9c3b
SilverFlame83 <imren_83@abv.bg>
2023-04-09 15:22:10 +0300

    search logic implemented

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -6,7 +6,6 @@ const user = require("../services/user");
 router.get("/catalog", async (req, res) => {
   const games = await req.storage.getAllGames();
 
-
   res.render("catalog", { games });
 });
 router.get("/create", async (req, res) => {
@@ -48,85 +47,102 @@ router.post("/create", isUser(), async (req, res) => {
 });
 
 router.get("/details/:id", async (req, res) => {
-    const game = await req.storage.getGameById(req.params.id);
-    
-    game.isOwner = req.user && req.user._id == game.owner._id;
-    game.isNotOwner = req.user && req.user._id != game.owner._id;
-    game.bought = req.user && game.boughtBy.find(u => u == req.user._id);
-   
-    res.render("details", { game });
-  });
+  const game = await req.storage.getGameById(req.params.id);
 
-  router.get("/delete/:id", isUser(), async (req, res) => {
-    try {
-      const game = await req.storage.getGameById(req.params.id);
-  
+  game.isOwner = req.user && req.user._id == game.owner._id;
+  game.isNotOwner = req.user && req.user._id != game.owner._id;
+  game.bought = req.user && game.boughtBy.find((u) => u == req.user._id);
+
+  res.render("details", { game });
+});
+
+router.get("/delete/:id", isUser(), async (req, res) => {
+  try {
+    const game = await req.storage.getGameById(req.params.id);
+
     //   if (game.owner != req.user._id) {
     //     throw new Error("Not the owner can not delete!");
     //   }
-      
-      await req.storage.deleteGame(req.params.id);
-      res.redirect("/game/catalog");
-    } catch (err) {
-      console.log(err.message);
-      res.redirect("/game/details/" + req.params.id);
-    }
-  }); 
 
-  router.get("/edit/:id", isUser(), async (req, res) => {
-    try {
-      const game = await req.storage.getGameById(req.params.id);
-  
-      // if (pet.owner != req.user._id) {
-      //   throw new Error("Not the owner can not edit!");
-      // }
-  
-      res.render("edit", { game });
-    } catch (err) {
-      console.log(err.message);
-      res.redirect("/game/details/" + req.params.id);
-    }
-  });
-
-  router.post("/edit/:id", isUser(), async (req, res) => {
-    try {
-    
-      await req.storage.editGame(req.params.id, req.body);
-        //TODO LOOK
-      res.redirect("/game/details/" + req.params.id);
-    } catch (err) {
-      const ctx = {
-        errors: parseError(err),
-        game: {
-          _id: req.params.id,
-          name: req.body.name,
-          imageUrl: req.body.imageUrl,
-          price: req.body.price,
-          description: req.body.description,
-          genre: req.body.genre,
-          platform: req.body.platform,
-        },
-      };
-      res.render('edit', ctx)
-    }
-  });
-
-  router.get('/buy/:id', isUser(), async (req,res)=>{
-    try {
-        //const buy = await req.storage.getGameById(req.params.id);
-        
-        //console.log('User',req.user)
-        await req.storage.buyGame(req.params.id, req.user._id);
-      
-        res.redirect('/game/details/' + req.params.id);
-    } catch (err) {
-        console.log('Éroor',err.message);
-        res.redirect('/game/details/'+ req.params.id)
-    }
+    await req.storage.deleteGame(req.params.id);
+    res.redirect("/game/catalog");
+  } catch (err) {
+    console.log(err.message);
+    res.redirect("/game/details/" + req.params.id);
+  }
 });
 
-// router.all('*', (req, res) => {
-//     res.render('404')
-//   })
+router.get("/edit/:id", isUser(), async (req, res) => {
+  try {
+    const game = await req.storage.getGameById(req.params.id);
+
+    // if (pet.owner != req.user._id) {
+    //   throw new Error("Not the owner can not edit!");
+    // }
+
+    res.render("edit", { game });
+  } catch (err) {
+    console.log(err.message);
+    res.redirect("/game/details/" + req.params.id);
+  }
+});
+
+router.post("/edit/:id", isUser(), async (req, res) => {
+  try {
+    await req.storage.editGame(req.params.id, req.body);
+    res.redirect("/game/details/" + req.params.id);
+  } catch (err) {
+    const ctx = {
+      errors: parseError(err),
+      game: {
+        _id: req.params.id,
+        name: req.body.name,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        description: req.body.description,
+        genre: req.body.genre,
+        platform: req.body.platform,
+      },
+    };
+    res.render("edit", ctx);
+  }
+});
+
+router.get("/buy/:id", isUser(), async (req, res) => {
+  try {
+    await req.storage.buyGame(req.params.id, req.user._id);
+
+    res.redirect("/game/details/" + req.params.id);
+  } catch (err) {
+    console.log("Éroor", err.message);
+    res.redirect("/game/details/" + req.params.id);
+  }
+});
+
+router.get("/search", async (req, res) => {
+  const games = await req.storage.getAllGames();
+
+  res.render("search", { games });
+});
+
+router.post("/search", async (req, res) => {
+  try {
+    const filter = await req.storage.getAllGames();
+
+    const search = req.body.search;
+    const platform = req.body.platform;
+
+    const games = filter.filter(
+      (game) =>
+        game.name.toLowerCase() == search.toLowerCase() &&
+        game.platform.toLowerCase() == platform.toLowerCase()
+    );
+
+    //console.log(games)
+    res.render("search", {games});
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 module.exports = router;
